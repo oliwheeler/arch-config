62a8abdbd1ef3d206e5aee3e04844bc443728bdc
Oliver Wheeler <olwheele@microsoft.com>
2018-10-28 20:26:01 +0100

    adding wallpaper script

---
+++ wallpaper.sh
@@ -0,0 +1,3 @@
+WALLPAPER_URL="https://wallpapershome.com/images/wallpapers/norway-3840x2160-5k-4k-wallpaper-hd-lofoten-islands-mountains-sea-5416.jpg"
+
+curl $WALLPAPER_URL -o ~/Pictures/wallpaper.jpg
