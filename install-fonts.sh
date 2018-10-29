334c38a4940a6097165b0310e42fdf1fc0644413
Oliver Wheeler <oliver.kuruma@gmail.com>
2018-10-29 11:22:33 +0100

    added fonts

---
+++ install-fonts.sh
@@ -0,0 +1,8 @@
+FONT_AWESOME="https://use.fontawesome.com/releases/v5.4.2/fontawesome-free-5.4.2-desktop.zip"
+
+curl $FONT_AWESOME -o ~/Downloads/font-awesome.zip
+
+unzip ~/Downloads/font-awesome.zip
+
+mv ~/Downloads/font-awesome/ /usr/share/fonts/
+
