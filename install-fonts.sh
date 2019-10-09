8d1614bb79964d60ff6f0f0c8ebc44515739fd95
Oliver Wheeler <oliver.kuruma@gmail.com>
2019-10-09 15:33:08 +0200

    Delete install-fonts.sh

--- install-fonts.sh
+++
@@ -1,8 +0,0 @@
-FONT_AWESOME="https://use.fontawesome.com/releases/v5.4.2/fontawesome-free-5.4.2-desktop.zip"
-
-curl $FONT_AWESOME -o ~/Downloads/font-awesome.zip
-
-unzip ~/Downloads/font-awesome.zip -d ~/Downloads/font-awesome
-
-sudo mv ~/Downloads/font-awesome/ /usr/share/fonts/
-
